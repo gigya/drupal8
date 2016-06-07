e4d2e15cdb6c828fc060f0666b9a81402721aae5
perelman.yuval <perelman.yuval@gmail.com>
2016-06-07 16:48:57 +0300

    add profile settings hook

--- gigya_raas/gigya_raas.api.php
+++ gigya_raas/gigya_raas.api.php
@@ -11,14 +11,27 @@
  */
 
 /**
+ * Modify the raas settings before it is added to the js.
+ *
+ * @param $raas_login
+ *   array with raas login params
+ * @param $raas_register
+ *   array with raas register params
+ */
+function hook_gigya_raas_settings_alter(array &$raas_login, array &$raas_register) {
+  $raas_login['screenSet'] = 'RegistrationLogin-A';
+}
+
+/**
  * Modify the data gigya_global_parameters before it is added to the js.
  *
  * @param $gigya_global_parameters
  *   An field map arrays.
  */
-function hook_gigya_raas_settings_alter(array &$raas_login, array &$raas_register) {
-  $raas_login['screenSet'] = 'RegistrationLogin-A';
+function hook_gigya_raas_profile_settings_alter(array &$raas_profile) {
+  $raas_profile['screenSet'] = 'Default-ProfileUpdate';
 }
+
 
 
 /**

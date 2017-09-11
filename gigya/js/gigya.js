5629096c3325705fdf6b42ecce05017c82086a57
inbal.zi <inbal@gigya-inc.com>
2017-09-11 14:14:27 +0300

    Moved setting of sessionExpiration to Raas module

--- gigya/js/gigya.js
+++ gigya/js/gigya.js
@@ -5,26 +5,21 @@
 
 (function ($, Drupal, drupalSettings) {
 
-  'use strict';
+    'use strict';
 
-  var init = function () {
-    //if value set in gigya raas module update the value of the sessionExpiration
-    if (drupalSettings.gigyaExtra) {
-        drupalSettings.gigya.globalParameters.sessionExpiration = (drupalSettings.gigyaExtra.session_type == "dynamic" ? -1 : drupalSettings.gigyaExtra.session_time);
-    }
-    window.__gigyaConf = drupalSettings.gigya.globalParameters;
+    var init = function () {
+        window.__gigyaConf = drupalSettings.gigya.globalParameters;
+        gigyaHelper.addGigyaScript(drupalSettings.gigya.apiKey, drupalSettings.gigya.lang, drupalSettings.gigya.dataCenter);
+        drupalSettings.gigya.isInit = true;
+    };
 
-    gigyaHelper.addGigyaScript(drupalSettings.gigya.apiKey, drupalSettings.gigya.lang, drupalSettings.gigya.dataCenter);
-    drupalSettings.gigya.isInit = true;
-  };
-
-  Drupal.behaviors.gigyaInit = {
-    attach: function (context, settings) {
-      if (!('isInit' in drupalSettings.gigya)) {
-        init();
-      }
-    }
-  };
+    Drupal.behaviors.gigyaInit = {
+        attach: function (context, settings) {
+            if (!('isInit' in drupalSettings.gigya)) {
+                init();
+            }
+        }
+    };
 
 
 })(jQuery, Drupal, drupalSettings);
